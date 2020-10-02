7864044be8e5497221bd2a6431bf7e884e06bc6e
ZeusDemiGod <56834555+ZeusDemiGod@users.noreply.github.com>
2020-10-02 16:12:59 +0530

    Add files via upload

---
+++ MinStack.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std ;
+typedef long long ll ;
+
+void push_element(stack<pair<int,int>> &st,int new_element){
+
+	int new_min=st.empty()?new_element:min(new_element,st.top().second) ;
+	st.push({new_element,new_min}) ;
+}
+
+void printstack(stack<pair<int,int>> st){
+
+	cout<<st.top().first<<" "<<st.top().second<<"\n" ;
+
+}
+
+int min_stack(stack<pair<int,int>> st){
+
+	int minimum=st.top().second ;
+	return minimum ;
+
+}
+
+
+
+void solve(){
+
+
+	stack<pair<int,int>> st ;
+
+	push_element(st,10) ;
+
+	printstack(st) ; 
+
+	push_element(st,20) ;
+	push_element(st,5) ;
+
+	push_element(st,30) ;
+
+	printstack(st) ; 
+
+	int ans1=min_stack(st) ;
+	cout<<ans1<<"\n" ;
+
+
+	int ans3=min_stack(st) ;
+	cout<<ans3<<"\n" ;
+
+	printstack(st) ;
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin) ;
+	freopen("output.txt", "w", stdout) ;
+#endif
+	
+
+	ll y ;
+
+
+	y=1 ;
+	//cin>>y ;
+
+	while(y--)
+	{		
+		solve() ;
+
+	}
+
+}

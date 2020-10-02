7cebb52666165ddac6bf9ba84f577e6b9b54e785
YourGoodFriendSP <71482137+YourGoodFriendSP@users.noreply.github.com>
2020-10-02 16:07:10 +0530

    Update BFSGrid.cpp

--- BFSGrid.cpp
+++ BFSGrid.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        //check for conditions        
+        if(grid.empty() ||  grid[0].empty()){
+            return 0 ;
+        }
+        
         
       
         int H=grid.size() ;
@@ -10,7 +15,11 @@ public:
         int answer=0 ;
         
         vector<pair<int,int>> directions={{1,0},{0,1},{-1,0},{0,-1}} ;
-     
+         
+        //check if it is inside the matrix
+        auto inside=[&](int row,int col){
+            return (0<=row && row<H && 0<=col && col<W) ;
+         };
         
         for(int row=0;row<H;row++){
             for(int col=0;col<W;col++){
@@ -32,7 +41,7 @@ public:
                             int new_row=p.first+dir.first ;
                             int new_col=p.second+dir.second ;
                             
-                            if(!visited[new_row][new_col] && grid[new_row][new_col]=='1'){
+                            if(inside(newrow,newcol) && !visited[new_row][new_col] && grid[new_row][new_col]=='1'){
                                 
                                 q.push(make_pair(new_row,new_col)) ;
                                 visited[new_row][new_col]=true ;
